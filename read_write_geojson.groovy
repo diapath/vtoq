a1985d4ee61ea6ee1002654c11e03f78e3f84399
Egor Zindy <ezindy@gmail.com>
2023-02-10 21:34:08 +0100

    Better handling of hierarchies and holes

--- read_write_geojson.groovy
+++ read_write_geojson.groovy
@@ -1,15 +1,20 @@
 // Code discussed in https://forum.image.sc/t/issue-with-accented-names-with-my-annotations-geojson-import-export-script-attached/74621
 import qupath.lib.scripting.QP
 
-def read_geojson()
+def read_geojson(removeAnnotations=true)
 {
     def server = QP.getCurrentImageData().getServer()
 
     // need to add annotations to hierarchy so qupath sees them
     def hierarchy = QP.getCurrentHierarchy()
-        
+    clearSelectedObjects(false);
+    
+    if (removeAnnotations)
+        removeObjects(getAnnotationObjects(), true)
+    
     //*********Get GeoJSON automatically based on naming scheme 
-    def path = GeneralTools.toPath(server.getURIs()[0]).toString()+".geojson";
+    def path = GeneralTools.toPath(server.getURIs()[0]).toString()
+    path = path+".geojson";
 
     def JSONfile = new File(path)
     if (!JSONfile.exists()) {
@@ -24,6 +29,32 @@ def read_geojson()
         annotation.setLocked(true)
         hierarchy.addPathObject(annotation) 
     }
+    
+    //Resolve Hierarchy first
+    resolveHierarchy()
+
+    //Rely on name and class to fin the holes
+    def holes = getAnnotationObjects().findAll {it.getPathClass()==getPathClass("Ignore") && it.getDisplayedName().equals("Hole")}
+
+    for (hole in holes) {
+        parent = hole.getParent()
+        if (parent == null)
+            continue;
+        parentGeom = parent.getROI().getGeometry()
+        holeGeom = hole.getROI().getGeometry()
+    
+        parentGeom = parentGeom.difference(holeGeom)
+        parentROI = GeometryTools.geometryToROI(parentGeom, ImagePlane.getDefaultPlane())
+        newParent = PathObjects.createAnnotationObject(parentROI, parent.getPathClass())
+        newParent.setLocked(true)
+        addObject(newParent)
+        removeObject(parent, true)    
+        removeObject(hole, true)    
+    }
+
+    //Resolve Hierarchy again since we've introduced new objects
+    resolveHierarchy()
+    fireHierarchyUpdate()    
 }
 
 def write_geojson()
@@ -47,4 +78,6 @@ def write_geojson()
 }
 
 //write_geojson()
-read_geojson()
+
+removeAnnotations=false
+read_geojson(removeAnnotations)
